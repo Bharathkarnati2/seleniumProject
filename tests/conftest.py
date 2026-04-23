d44c879692c5d47efd8412d6e5ec54d6a12099ab
Bharath <bharathkarnati2@gmail.com>
2026-04-23 15:59:48 +0530

    Conftest_update

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,6 +21,11 @@ def driver(request):
             "profile.password_manager_leak_detection": False
         }
 
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+
         chrome_options.add_experimental_option("prefs", prefs)
         chrome_options.add_argument("--disable-notifications")
         chrome_options.add_argument("--disable-infobars")
