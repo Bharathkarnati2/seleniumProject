6ae0b3106db9c6dab89a441eabaf10effd444bc2
Bharath <bharathkarnati2@gmail.com>
2026-04-23 18:51:35 +0530

    checkoutPage

--- utills/utils.py
+++ utills/utils.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def wait_for_clickable(driver, locator):
-    wait = WebDriverWait(driver, 20)
-    return wait.until(EC.element_to_be_clickable(locator))
+def wait_for_clickable(driver, locator, timeout=25):
+    wait = WebDriverWait(driver, timeout)
 
-# def wait_for_visible(driver, locator):
-#     wait = WebDriverWait(driver, 10)
-#     return wait.until(EC.element_to_be_visble(locator))
+    element = wait.until(EC.presence_of_element_located(locator))
+    wait.until(EC.visibility_of(element))
+
+    return element
