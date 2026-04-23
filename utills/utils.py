b60ffdf1b6e34a73aec23a9c61d2f918db2c899f
Bharath <bharathkarnati2@gmail.com>
2026-04-23 18:42:33 +0530

    utils

--- utills/utils.py
+++ utills/utils.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def wait_for_clickable(driver, locator):
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)
     return wait.until(EC.element_to_be_clickable(locator))
 
 # def wait_for_visible(driver, locator):
